6fdc1163a256e0a1e8c8a6e9acb61b1df1c3ca2b
Eugene <checkmaten1@gmail.com>
2019-08-10 19:31:47 +0300

    work on prompter handler

--- mephisto_socket.js
+++ mephisto_socket.js
@@ -21,17 +21,9 @@ const tokens = Object.freeze({
 
 const sequenceNumberByClient = {};
 
-const sendPrompt = (clientID, prompt) => {
-    for (let key in sequenceNumberByClient) {
-        const client = sequenceNumberByClient[key].client;      // clientID instead of key
-        client.emit('prompt', { prompt: 'testPrompt' });
-    }
-};
-
-setInterval(() => {
-    console.log('trying to emit test prompt');
-    sendPrompt();
-}, 5000);
+// const sendPrompt = (client, prompt) => {
+//     client.emit('prompt', { prompt });
+// };
 
 
 // event fired every time a new client connects:
@@ -47,108 +39,6 @@ io.on('connection', client => {
             client.emit('authorizationSuccess');
             // initialize this client's sequence number
             sequenceNumberByClient[client.id] = { client, token };
-
-            const testNickname = 'testTEST';
-            let currentPrompt =
-                `<div class="main-container spins party-poker">
-    <div class="player player0">
-        <div class="nickname green">Joe <span class="balance">/ 23bb</span></div>
-        <div>
-            <span class="stat green">VPIP: 54, </span><span class="stat">PFR: 19, </span><span class="stat">3Bet: 13</span>
-        </div>
-        <div>
-            <span class="stat">CBet: 45, </span><span class="stat green">Raise%: 9, </span><span class="stat">Call%: 55</span>
-        </div>
-        <div class="dealer"><span>D</span></div>
-        <div class="amount bet-raise">bet: 5bb</div>
-    </div>
-    <div class="player player1">
-        <div class="nickname red">checkmate <span class="balance">/ 16bb</span></div>
-        <div>
-            <span class="stat red">VPIP: 17, </span><span class="stat">PFR: 16, </span><span class="stat">3Bet: 20</span>
-        </div>
-        <div>
-            <span class="stat">CBet: 65, </span><span class="stat red">Raise%: 25, </span><span class="stat">Call%: 42</span>
-        </div>
-        <div class="amount check-call">check</div>
-    </div>
-    <div class="player player2">
-        <div class="nickname">See my luck <span class="balance">/ 27bb</span></div>
-        <div>
-            <span class="stat">VPIP: 30, </span><span class="stat">PFR: 23, </span><span class="stat">3Bet: 15</span>
-        </div>
-        <div>
-            <span class="stat">CBet: 55, </span><span class="stat">Raise%: 12, </span><span class="stat">Call%: 40</span>
-        </div>
-    </div>
-    <div class="board">
-        <div class="pot">Pot: 10bb</div>
-        <div class="card spades">
-            <div class="value">A</div>
-            <div class="suit">&#9824</div>
-        </div>
-        <div class="card clubs">
-            <div class="value">8</div>
-            <div class="suit">&#9827</div>
-        </div>
-        <div class="card hearts">
-            <div class="value">T</div>
-            <div class="suit">&#9829</div>
-        </div>
-        <div class="card diamonds">
-            <div class="value">Q</div>
-            <div class="suit">&#9830</div>
-        </div>
-        <div class="card diamonds">
-            <div class="value">2</div>
-            <div class="suit">&#9830</div>
-        </div>
-    </div>
-    <div class="hero-hand">
-        <div class="card diamonds">
-            <div class="value">T</div>
-            <div class="suit">&#9830</div>
-        </div>
-        <div class="card spades">
-            <div class="value">T</div>
-            <div class="suit">&#9824</div>
-        </div>
-    </div>
-    <div class="prompt">
-        <div class="bet-raise red">
-            Raise: 25bb
-        </div>
-        <div class="diagram">
-            <div class="fold" style="width: 10%"></div>
-            <div class="check-call" style="width: 35%"></div>
-            <div class="bet-raise" style="width: 55%"></div>
-        </div>
-        <div class="sizings">
-            <table>
-                <tr style="opacity: 0.25">
-                    <td class="check-call">Call</td>
-                    <td class="ev">EV: 5bb</td>
-                </tr>
-                <tr style="opacity: 0.3">
-                    <td class="bet-raise">Raise: 1pot</td>
-                    <td class="ev">EV: 10bb</td>
-                </tr>
-                <tr style="opacity: 0.5">
-                    <td class="bet-raise">Raise: 1.6pot</td>
-                    <td class="ev">EV: 13bb</td>
-                </tr>
-                <tr>
-                    <td class="bet-raise">Raise: 2.5pot</td>
-                    <td class="ev">EV: 15bb</td>
-                </tr>
-                <tr style="opacity: 0.7">
-                    <td class="bet-raise">All-in</td>
-                    <td class="ev">EV: 14bb</td>
-                </tr>
-            </table>
-        </div>
-    </div>
-</div>`;
 
             // config
             client.on('getConfig', () => {
@@ -187,17 +77,15 @@ io.on('connection', client => {
                     console.log(data);
                     client.emit('frameSuccess', data.id);
 
-                    // test prompt
-                    client.emit('prompt', currentPrompt);
-
                     const prompterData = {
                         request: {
                             requestType: 'prompter',
                         },
                         data,
+                        client,
                     };
 
-                    sessionsHandler.sessionsListener(token, client.id, prompterData);
+                    sessionsHandler.sessionsListener(token, data.id, prompterData);     // data.id == table id from recognition
                 } else {
                     client.emit('frameError', data);
                 }
@@ -225,15 +113,11 @@ io.on('connection', client => {
 
             client.on('disconnect', () => {
                 delete sequenceNumberByClient[client.id];
-
-                // sequenceNumberByClient.delete(client.id);
                 console.info(`Client gone [${client.id}]`);
             });
         }
     });
 });
-
-module.exports.sendPrompt = sendPrompt;
 
 
 // const sessionsHandler = require('./sessionsHandler');
